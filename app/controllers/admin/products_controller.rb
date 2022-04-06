40bd309c866244924b988bff78feeded14ef34b4
James Murphy <jamesm8228@gmail.com>
2022-04-06 16:07:36 -0400

    Add lint changes

--- app/controllers/admin/products_controller.rb
+++ app/controllers/admin/products_controller.rb
@@ -1,6 +1,4 @@
 class Admin::ProductsController < ApplicationController
-  # http_basic_authenticate_with username: ENV["USERNAME"],
-  # password: ENV["PASSWORD"]
   http_basic_authenticate_with name: ENV["USERNAME"], password: ENV["PASSWORD"]
   def index
     @products = Product.order(id: :desc).all
